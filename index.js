678e4816ecd8bc836fb05a07a3599befaf2a00d6
Dauren Bablanov <bablanov.dauren@yandex.kz>
2023-05-28 14:30:32 +0600

    init cli library

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 require("dotenv").config();
 
+const init = require('./cmd/utils/init');
+const cli = require('./cmd/utils/cli');
+const log = require('./cmd/utils/log');
+
+const input = cli.input;
+const flags = cli.flags;
+const { clear, debug } = flags;
+
+(async () => {
+  init({ clear });
+  input.includes(`help`) && cli.showHelp(0);
+
+  debug && log(flags);
+})();
